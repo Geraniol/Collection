74b20145d5529436bbac578098d7e5b0ca86b906
Geraniol <1215918624@qq.com>
2022-11-10 02:13:07 +0800

    2022 1110 0213

--- drag.py
+++ drag.py
@@ -4,7 +4,9 @@ with open(sys.path[0]+"/drag_sample.html", "rt")as f:
     content = f.read()
 new = ""
 
-for cp, dns, fns in os.walk("/Volumes/Exchange/Programs/Projects_HTML/geraniol.github.io/Pages/Public/img/pv"):
+url = ""
+
+for cp, dns, fns in os.walk(url):
     for fn in fns:
         new += '\n<img id="'+fn+'" src="'+cp+'/'+fn + \
             '" draggable="true" ondragstart="drag(event)">\n'
